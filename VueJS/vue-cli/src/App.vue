d86a6217e376c552f0619fa4de30947e637f7833
Jack Daniels <jndaniels@me.com>
2020-09-21 11:28:00 +0100

    assignemnt 6

--- VueJS/vue-cli/src/App.vue
+++ VueJS/vue-cli/src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    <img src="./assets/logo.png">
-    <h1>Hello World</h1>
-    <app-servers></app-servers>
-  </div>
+    <div class="container">
+        <app-header></app-header>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Servers from './Servers.vue'
+import ServerDetails from './ServerDetails.vue'
+export default {
+    components: {
+      'app-footer': Footer,
+      'app-header': Header,
+      'app-servers': Servers,
+      'app-server-details': ServerDetails
+    }
+}
 </script>
 
 <style>
