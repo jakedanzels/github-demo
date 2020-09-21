535a1b4bb666eed1f9cee2d3099533ba0395097c
Jack Daniels <jndaniels@me.com>
2020-09-21 14:56:34 +0100

    centralised eventBus code

--- VueJS/comp-comms/src/components/UserEdit.vue
+++ VueJS/comp-comms/src/components/UserEdit.vue
@@ -18,7 +18,8 @@
             editAge() {
                 this.userAge = 30;
                 //this.$emit('ageWasEdited', this.userAge);
-                eventBus.$emit('ageWasEdited', this.userAge);
+                //eventBus.$emit('ageWasEdited', this.userAge);
+                eventBus.changeAge(this.userAge);
             }
         }
     }
