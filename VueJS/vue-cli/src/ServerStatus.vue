d87a103500a1aaa4484b61e8000af9f3967fc3e8
Jack Daniels <jndaniels@me.com>
2020-09-21 10:54:57 +0100

    lecture 422

---
+++ VueJS/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <hr>
+    <p> Server Status: {{ status }} </p>
+    <button @click="changeStatus">Change Status</button>
+</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            status: 'Critial'
+        }
+    },
+    methods: {
+        changeStatus() {
+            this.status = 'Normal';
+        }
+    }
+}
+</script>
