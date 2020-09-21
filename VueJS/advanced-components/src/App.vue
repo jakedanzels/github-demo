8bd1534b1b91b6eee30efe0dd65b36d46cc6e529
Jack Daniels <jndaniels@me.com>
2020-09-21 23:30:27 +0100

    quotes app

---
+++ VueJS/advanced-components/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="addNewQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <hr>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Click on a quote to delete it!</div> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Example Quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+         methods: {
+            addNewQuote(data) {
+                this.quotes.push(data);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index,1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
