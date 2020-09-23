4f63f013b9eb80615eeb05afe01646cf4a98af71
Jack Daniels <jndaniels@me.com>
2020-09-23 19:44:55 +0100

    working post get http

--- VueJS/http/src/App.vue
+++ VueJS/http/src/App.vue
@@ -3,6 +3,19 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                    <label>Mail</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                    <br>
+                    <button class="btn btn-primary" @click="submit">Submit</button>
+                    <hr>
+                    <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                    <ul class="list-group">
+                        <li class="list-group-item" v-for="u in users" :key="u.username">{{ u.username}} - {{ u.email }}</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -10,6 +23,45 @@
 
 <script>
     export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                },
+                users: [],
+                resource: {}
+            }
+        },
+        methods: {
+            submit() {
+                //console.log(this.user);
+                // this.$http.post('data.json', this.user)
+                //     .then(response => {
+                //         console.log(response)
+                //     }, error => {
+                //         console.log(error)
+                //     });
+                this.resource.save({}, this.user);
+            },
+            fetchData() {
+                this.$http.get('data.json')
+                .then(response => {
+                    return response.json();
+                    console.log(data);
+                })
+                .then(data => {
+                    const resultArray = [];
+                    for (let key in data){
+                        resultArray.push(data[key]);
+                    }
+                    this.users = resultArray;
+                });
+            }
+        },
+        created() {
+            this.resource = this.$resource('data.json');
+        }
     }
 </script>
 
--- VueJS/http/src/main.js
+++ VueJS/http/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://vuejs-http-dd715.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);   
+});
 
 new Vue({
   el: '#app',
