8cd11e9aeca949fa3572ff7354fe0284a3d5a134
Jack Daniels <jndaniels@me.com>
2020-11-12 11:54:13 +0000

    route transisitons

--- VueJS/red-book/src/App.vue
+++ VueJS/red-book/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <nav-bar></nav-bar>
-  <transition>
-  <router-view></router-view>
-  </transition>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
   <my-footer></my-footer>
 </template>
 
@@ -41,27 +43,24 @@ body {
   margin: 0;
 }
 
-.route-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
-}
-
+.route-enter-from,
 .route-leave-to {
   opacity: 0;
-  transform: translateY(30px);
 }
 
+
 .route-enter-active {
-  transition: all 0.3s ease-out;
+  /* animation: slide-scale 0.4s ease-out; */
+ transition: opacity 0.5s ease-out;
 }
 
 .route-leave-active {
-  transition: all 0.3s ease-in;
+  /* animation: slide-scale 0.4s ease-in; */
+  transition: opacity 0.5s ease-in;
 }
 
 .route-enter-to,
 .route-leave-from {
   opacity: 1;
-  transform: translateY(0);
 }
 </style>
